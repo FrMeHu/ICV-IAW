6fea139cbfacd267f7466269b5ddc78011d1852b
FrMeHu <fmenhua603@g.educaand.es>
2024-12-07 21:27:27 +0100

    Actividad4

---
+++ Actividad4/nextasir/app/btopa/page.jsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>btopa</h1>
+            <h1>btopa</h1>
+            <h1>btopa</h1>
+            <h1>btopa</h1>
+        </div>
+    );
+}
---
+++ Actividad4/nextasir/app/contacto/page.jsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Contacto</h1>
+            <h1>Contacto</h1>
+            <h1>Contacto</h1>
+            <h1>Contacto</h1>
+        </div>
+    );
+}
